ea102d77ff740018987060c131bf47bb8bb58070
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-10-02 16:56:14 +0530

    Add Operator Functionality

--- src/App.js
+++ src/App.js
@@ -18,7 +18,12 @@ class App extends Component {
     }
 
     addToCurrent = (symbol) => {
-        this.setState({current: this.state.current + symbol});
+        if(["/", "-", "+", "X"].indexOf(symbol) > -1) {
+            let {previous} = this.state;
+            previous.push(this.state.current + symbol);
+            this.setState(previous);
+        }else
+            this.setState({current: this.state.current + symbol});
     }
 
     render() {
@@ -45,6 +50,11 @@ class App extends Component {
 
         return(
             <div className="App">
+            { this.state.previous.length > 0 ? 
+                <div className="float-last"> {
+                    this.state.previous[this.state.previous.length-1]
+                    } </div> 
+                    : null}
 
               <input className = "result" type = "text" value = {this.state.current}/>
 
